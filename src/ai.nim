5a1870d7b76ad7b0b695fe910f57f845c8dc72e5
kaneko_takayuki <g.knk.9410@gmail.com>
2018-03-26 09:21:06 +0900

    style: 探索アルゴリズムの結果用構造体を用意

--- src/ai.nim
+++ src/ai.nim
@@ -1,3 +1,4 @@
+import dto.searchResult
 from reversi_core import getPutBoard
 from reversi_core import getRevBoard
 from game import isEnd
@@ -14,14 +15,14 @@ proc evaluate(): int
 ## @param depth: 先読みする深さ
 ##
 ## result: int 現盤面から先読みした最大値
-proc negaScout(me: uint64, op: uint64, alpha: int, beta: int, depth: int): int =
+proc negaScout(me: uint64, op: uint64, alpha: int, beta: int, depth: int): SearchResult =
   # 先読み深さが規定値に到達したので評価して返す
   if depth == 0:
-    return evaluate()
+    return SearchResult(value: evaluate(), pos: -1)
   
   # 両者が置けない状況になったらゲームは終了なので、石数で評価して返す
   if isEnd(me, op):
-    return evaluate()
+    return SearchResult(value: evaluate(), pos: -1)
   
   # 石が置けない時、パスして探索を続ける
   let enablePut = getPutBoard(me, op)
@@ -29,7 +30,7 @@ proc negaScout(me: uint64, op: uint64, alpha: int, beta: int, depth: int): int =
     return -negaScout(op, me, -beta, -alpha, depth)
   
   # 探索で使う
-  result = -Inf.int              # 先読みした結果、最大の値を返すようにしたい
+  result = SearchResult(value: Inf.int, pos: -1)  # 先読みした結果、評価の最大値とその時の手を返す
   var childAlpha: int = alpha
 
   # 置ける場所について順番にシミュレーション
@@ -39,26 +40,24 @@ proc negaScout(me: uint64, op: uint64, alpha: int, beta: int, depth: int): int =
       continue
     
     # i番目のポジションに置いて検証
-    let 
+    let
       pos: uint64 = 1'u shl i
       rev: uint64 = getRevBoard(me, op, pos)
       childMe: uint64 = me xor (pos or rev)
       childOp: uint64 = op xor rev
-      value: int = -negaScout(childOp, childMe, -beta, -childAlpha, depth - 1)
+      childResult: SearchResult = -negaScout(childOp, childMe, -beta, -childAlpha, depth - 1)
 
     # 枝刈り
-    if beta <= value:
-      return value
+    if beta <= childResult.value:
+      return childResult
     
     # α値の更新
-    if childAlpha < value:
-      childAlpha = value
+    if childAlpha < childResult.value:
+      childAlpha = childResult.value
     
     # 最大値の更新
-    if result < value:
-      result = value
-
-
+    if result < childResult:
+      result = childResult
 
 
 ## TODO: 評価関数はまだ仮
